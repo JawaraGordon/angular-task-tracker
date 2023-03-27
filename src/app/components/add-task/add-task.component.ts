0ad89ae7d217d2211ed3d9903c2928e250546a95
JawaraGordon <jawaragordon@icloud.com>
2023-03-27 19:23:36 -0400

    created form submit with alert, added form styling

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css']
 })
-export class AddTaskComponent {
+export class AddTaskComponent implements OnInit{
+  text: string;
+  day: string;
+  reminder: boolean = false;
+
+  constructor(){}
+
+  ngOnInit(): void {}
+
+  onSubmit(){
+    if (!this.text){
+      alert('Please add a task');
+      return
+    }
+  }
 
 }
