6bb4a23dacd58de4339faaed28bd9e3440d410bb
JawaraGordon <jawaragordon@icloud.com>
2023-03-28 17:35:37 -0400

    finished add task, tested for persistence against the json server

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -34,6 +34,6 @@ export class TasksComponent implements OnInit{
   }
 
   addTask(task: Task){
-    console.log(task)
+    this.taskService.addTask(task).subscribe((task)=>(this.tasks.push(task)))
   }
 }
