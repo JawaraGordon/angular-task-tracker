737562fae835083282a20c3ccf9d1b18dfd14c83
JawaraGordon <jawaragordon@icloud.com>
2023-03-29 18:00:49 -0400

    created footer component, styling and route to about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,19 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './pages/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 
 const appRoutes: Routes = [
   {
     path: '',
     component: TasksComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   }
 ]
 
@@ -27,7 +34,10 @@ const appRoutes: Routes = [
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent,
+   
   ],
   imports: [
     BrowserModule,
